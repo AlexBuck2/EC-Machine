c71e212e9db0f28a9158034e502f1bb28ca2424b
AlexBuck2 <145092421+AlexBuck2@users.noreply.github.com>
2023-10-16 21:46:20 -0700

    v1
    Signed-off-by: AlexBuck2 <145092421+AlexBuck2@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,36 @@
 import java.util.ArrayList;
-
+import java.util.Arrays;
 public class Main {
-
     public static void main(String[] args) {
-        ArrayList<Integer> ecMachine = new ArrayList<Integer>();
-        ecMachine.add(1);
-        ecMachine.add(0);
-        ecMachine.add(1);
-        ecMachine.add(3);
-        ecMachine.add(0);
-        ecMachine.add(0);
-        // ecMachine.add();
-        // ecMachine.add();
-        //System.out.println("Hello world!");
-        int n = 0;
-        while(ecMachine.get(n) != 99 && n < ecMachine.size()){
-            if(ecMachine.get(n) == 1){
-            ifOne(ecMachine, n);
-            }
-            else if(ecMachine.get(n) == 2){
-                ifTwo(ecMachine, n);
-            }
-            n++;
+        int []ecMachine = new int []{1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,1,10,19,1,19,5,23,2,23,9,27,1,5,27,31,1,9,31,35,1,35,10,39,2,13,39,43,1,43,9,47,1,47,9,51,1,6,51,55,1,13,55,59,1,59,13,63,1,13,63,67,1,6,67,71,1,71,13,75,2,10,75,79,1,13,79,83,1,83,10,87,2,9,87,91,1,6,91,95,1,9,95,99,2,99,10,103,1,103,5,107,2,6,107,111,1,111,6,115,1,9,115,119,1,9,119,123,2,10,123,127,1,127,5,131,2,6,131,135,1,135,5,139,1,9,139,143,2,143,13,147,1,9,147,151,1,151,2,155,1,9,155,0,99,2,0,14,0};
+        ecCalculator(ecMachine);
+        for(int i=0;i<ecMachine.length;i++) {
+            System.out.println(ecMachine[i]);
         }
-       for(int q =0; q < ecMachine.size(); q++)
-            System.out.print(ecMachine.get(q));
+    }
+    public static int[] ecCalculator(int[] ecMachine) {
+        ecMachine[1] = 12;
+        ecMachine[2] = 2;
 
+        for (int i = 0; i < ecMachine.length; i += 4) {
+            int intialIndex = ecMachine[i];
+            int index2 = ecMachine[i + 1];
+            int index3 = ecMachine[i + 2];
+            int positionIndex = ecMachine[i + 3];
+
+            if (intialIndex == 1) {
+                ecMachine[positionIndex] = ecMachine[index2] + ecMachine[index3];
+            } else if (intialIndex == 2) {
+                ecMachine[positionIndex] = ecMachine[index2] * ecMachine[index3];
+            } else if (intialIndex == 99) {
+                break;
+            }
+        }
+        return ecMachine;
     }
 
-    public static void ifOne(ArrayList<Integer> ecMachine, int index){
+}
+    /**public static void ifOne(ArrayList<Integer> ecMachine, int index){
         int sum = ecMachine.get(ecMachine.get(index+1)) + ecMachine.get(ecMachine.get(index+2));
         ecMachine.set(index + 3, sum);
     }
@@ -36,4 +38,5 @@ public class Main {
         int product = ecMachine.get(ecMachine.get(index+1)) * ecMachine.get(ecMachine.get(index+2));
         ecMachine.set(index + 3, product);
     }
-}
+
+     */
